d56346e549d0806b258f443d7b96fb7cb1a38728
berwyn <berwyn.codeweaver@gmail.com>
2016-02-08 21:03:08 -0700

    Basic app component!

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,25 @@
+import { Component } from 'angular2/core';
+import { NgFor } from 'angular2/common';
 
+@Component({
+    selector: 'app',
+    template: `
+        <div class="ngut-app">
+            <ul>
+                <li *ngFor="#todo of todos">{{ todo.text }}</li>
+            </ul>
+        </div>
+    `,
+    directives: [
+        NgFor
+    ]
+})
 export class App {
+    
+    todos: any[] = [
+        { text: 'Fight Crime', done: false },
+        { text: 'Clean Batcave', done: false },
+        { text: 'Fix hole in cowl', done: false }
+    ];
     
 }
